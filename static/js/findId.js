26dd97afa73dcdf512e4144e1743f28f1c2265fe
jeong0300 <dltnwjd8898@naver.com>
2025-02-12 00:52:55 +0900

    Feat: 완료

--- static/js/findId.js
+++ static/js/findId.js
@@ -9,10 +9,19 @@ function find() {
   const middleNum = userDataAll.filter((user) => user.middleNumber === middle);
   const lastNum = userDataAll.filter((user) => user.lastNumber === last);
 
-  if (areaNum.length === 0 || middleNum.length === 0 || lastNum.length === 0) {
+  if (area === "" || middle === "" || last === "") {
     Swal.fire({
       icon: "error",
-      text: "작석된 번호로 가입된 유저가 없습니다.",
+      text: "전화번호를 정확히 적어주세요.",
+    });
+  } else if (
+    areaNum.length === 0 ||
+    middleNum.length === 0 ||
+    lastNum.length === 0
+  ) {
+    Swal.fire({
+      icon: "error",
+      text: "해당 번호로 가입된 유저가 없습니다.",
     });
   } else {
     const user = userDataAll.find(
--- static/js/findPw.js
+++ static/js/findPw.js
@@ -4,26 +4,24 @@ function find() {
 
   const userDataAll = JSON.parse(localStorage.getItem("userData"));
 
-  const areaNum = userDataAll.filter((user) => user.areaCode === area);
-  const middleNum = userDataAll.filter((user) => user.middleNumber === middle);
-  const lastNum = userDataAll.filter((user) => user.lastNumber === last);
+  const idNum = userDataAll.filter((user) => user.id === id);
 
-  if (areaNum.length === 0 || middleNum.length === 0 || lastNum.length === 0) {
+  if (id === "") {
     Swal.fire({
       icon: "error",
-      text: "작석된 번호로 가입된 유저가 없습니다.",
+      text: "아이디가 비어있습니다",
+    });
+  } else if (idNum.length === 0) {
+    Swal.fire({
+      icon: "error",
+      text: "해당 아이디로 가입된 유저가 없습니다.",
     });
   } else {
-    const user = userDataAll.find(
-      (user) =>
-        user.areaCode === area &&
-        user.middleNumber === middle &&
-        user.lastNumber === last
-    );
+    const user = userDataAll.find((user) => user.id === id);
 
     Swal.fire({
       icon: "success",
-      text: `해당 유저의 아이디는 ${user.id} 입니다.`,
+      text: `해당 유저의 비밀번호는 ${user.pass} 입니다.`,
     });
   }
 }
